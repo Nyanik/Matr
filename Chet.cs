29d04bd45edc93bf3f546e967da24afce3580e77
мелузовна <@TS-05>
2021-09-10 15:40:59 +0300

    Добавьте файлы проекта.

---
+++ Chet.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Matrix
+{
+    // Данный класс создан с целью произведения вычисления
+    // и подсчета чисел чья сумма кубов равна сумме цифр числа
+    class Chet
+    {
+        string readFile, savingFile;        
+        int[,] matr; 
+        int count = 0; 
+        List<int> neededNum = new List<int>(); 
+
+        public Chet(string readFile, string savingFile) 
+        {
+            this.readFile = readFile;
+            this.savingFile = savingFile;
+        }
+        bool NumSumCheck(int num) 
+        {
+            int sum = 0, origNum = num;
+            bool result = false;
+            while (num > 0)
+            {
+                sum = sum + Convert.ToInt32(Math.Pow(num % 10, 3));
+                num = num / 10;
+            }
+
+            if (sum == origNum) result = true;
+
+            return result;
+        }
+        void MatrixMoving() 
+        {  
+            foreach(int num in matr)
+            {
+                if(NumSumCheck(num) == true)
+                {
+                    count++;
+                    neededNum.Add(num);
+                }
+            }
+        }   
+        //Чтение матрицы из файла и вывод результата в текстовы файл
+        public void CalcStart() 
+        {
+            ReadWrite.Read(readFile, ref matr);
+            MatrixMoving();
+            ReadWrite.Write(savingFile, neededNum, count);
+        }
+    }
+
+}
---
+++ ReadWrite.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace Matrix
+{
+    //Определение длины строки массива
+    //Инициализируется массив, куда будут записываться значения
+    class ReadWrite
+    {
+        public static void Read(string path, ref int[,] matrix)
+        {
+
+            if (!File.Exists(path))
+            {
+                Console.WriteLine("Файл не найден!");
+                Console.ReadKey();
+                Environment.Exit(0);
+            }
+            var lines = File.ReadAllLines(path);
+            int k = 0;
+            string[] strLen = lines[0].Split(';'); 
+            matrix = new int[lines.Length, strLen.Length]; 
+            foreach (var line in lines)
+            {
+
+                string[] str = line.Split(';'); 
+                for (int j = 0; j < str.Length; j++)
+                {
+                    matrix[k, j] = Convert.ToInt32(str[j]); 
+                }
+                k++;
+            }
+        }
+
+        public static void Write(string path, List<int> neededNums, int count)
+        {
+            if (!File.Exists(path)) File.Create(path).Close();
+            using (StreamWriter sw = new StreamWriter(path, false))
+            {
+
+                sw.WriteLine("Элементы массива, соответсвующие условия задачи:");
+                foreach(var num in neededNums)
+                {
+                    sw.Write(num+"; ");
+                }
+                sw.WriteLine("\nОбщее количество элементов, подходящие по условию задачи: " +count);
+            }
+        }
+    }
+}
